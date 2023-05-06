dcba33686c1d6d17a388ce1b6a4db0b8ce5cd79c
Julian Feregotti <julianferegotti96@gmail.com>
2023-05-06 16:51:47 -0300

    removed sidebar and placeholder for calendar

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,5 +3,6 @@ import Sidebar from "./Sidebar";
 import Card from "./Card";
 import DolarCard from "./DolarCard";
 import WorkDaysCard from "./WorkDaysCard";
+import Calendar from "./Calendar";
 
-export { Layout, Sidebar, Card, DolarCard, WorkDaysCard };
+export { Layout, Sidebar, Card, DolarCard, WorkDaysCard, Calendar };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,24 +1,18 @@
-import { DolarCard, WorkDaysCard } from 'components'
-import './styles.css'
-import { Grid } from '@mui/material'
+import { DolarCard, WorkDaysCard } from "components";
+import "./styles.css";
+import { Grid, Stack } from "@mui/material";
 
 const Home = () => {
   return (
-    <Grid container spacing={2} padding={4} >
+    <Grid container spacing={2} padding={4}>
       <Grid item xs={2} md={4}>
-        <DolarCard />
-      </Grid>
-      <Grid item xs={2} md={4}>
-        <WorkDaysCard />
-      </Grid>
-      <Grid item xs={2} md={4}>
-        <WorkDaysCard />
-      </Grid>
-      <Grid item xs={2} md={20}>
-        <WorkDaysCard />
+        <Stack spacing={1}>
+          <DolarCard />
+          <WorkDaysCard />
+        </Stack>
       </Grid>
     </Grid>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
