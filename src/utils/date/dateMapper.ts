ca8ae9434450e4467b4685f7ed473d99bd1cc908
Julian Feregotti <julianferegotti96@gmail.com>
2023-05-06 23:39:10 -0300

    added more utility functions

--- src/utils/date/dateMapper.ts
+++ src/utils/date/dateMapper.ts
@@ -1,26 +1,46 @@
 const MONTH = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 
-const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const DAYS = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 const getMonthName = (date: Date): string => {
-  return MONTH[date.getMonth()]
-}
+  return MONTH[date.getMonth()];
+};
 
 const getDayName = (date: Date): string => {
-  return DAYS[date.getDate()]
-}
+  return DAYS[date.getDate()];
+};
 
-export {getDayName, getMonthName}
+const getDateObject = () => {
+  const currentDate = new Date();
+
+  return {
+    day: currentDate.getDate(),
+    month: currentDate.getMonth() + 1,
+    year: currentDate.getFullYear(),
+    dayName: getDayName(currentDate),
+    monthName: getMonthName(currentDate),
+  };
+};
+
+export { getDayName, getMonthName, getDateObject };
