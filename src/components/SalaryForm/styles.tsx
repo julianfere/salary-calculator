ca2200be5648636e0239c5ddbd169e4536a36f62
Julian Feregotti <julianferegotti96@gmail.com>
2024-01-18 23:19:16 -0300

    Refactor component imports and add responsive styles

--- src/components/SalaryForm/styles.tsx
+++ src/components/SalaryForm/styles.tsx
@@ -1,6 +1,12 @@
-import { Form } from "antd";
+import { Button, Form } from "antd";
 import styled from "styled-components";
 
 export const StyledFormItem = styled(Form.Item)`
   display: inline-block;
+`;
+
+export const StyledButton = styled(Button)`
+  @media (max-width: 570px) {
+    width: 100%;
+  }
 `;
