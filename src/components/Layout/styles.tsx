ca89fdc047144a512bc1e4a907dcb778ca3cc923
Julian Feregotti <julianferegotti96@gmail.com>
2024-03-01 22:34:43 -0300

    patch

--- src/components/Layout/styles.tsx
+++ src/components/Layout/styles.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Header } from "antd/es/layout/layout";
+//@ts-ignore
 import { ReactComponent as MenuIcon } from "@assets/icons/menu.svg";
 
 export const StyledHeader = styled(Header)`
